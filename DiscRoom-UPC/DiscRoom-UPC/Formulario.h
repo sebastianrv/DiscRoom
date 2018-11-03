4c0c1b7251b18dcfd2cb9ef72ef36dd77aefedf5
sebastian <romero_villa-sebastian@hotmail.com>
2018-11-03 12:20:11 -0500

    actualizacion de cabeceras

--- DiscRoom-UPC/DiscRoom-UPC/Formulario.h
+++ DiscRoom-UPC/DiscRoom-UPC/Formulario.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "Juego.h"
+#include "Jugador.h"
 namespace DiscRoomUPC {
 
 	using namespace System;
@@ -14,9 +16,19 @@ namespace DiscRoomUPC {
 	/// </summary>
 	public ref class Formulario : public System::Windows::Forms::Form
 	{
+		CJuego * objJuego = new CJuego();
+
+	public:
+		Bitmap ^ bmpNivel1 = gcnew Bitmap("todaimagen\\MapaNivel1.png");
+	private: System::Windows::Forms::Timer^  timer1;
+	public:
+		Bitmap ^ bmpBatman = gcnew Bitmap("todaImagen\\personajeBatman.png");
+
 	public:
 		Formulario(void)
 		{
+			bmpBatman->MakeTransparent(bmpBatman->GetPixel(0, 0));
+
 			InitializeComponent();
 			//
 			//TODO: agregar código de constructor aquí
@@ -34,12 +46,14 @@ namespace DiscRoomUPC {
 				delete components;
 			}
 		}
+	private: System::ComponentModel::IContainer^  components;
+	protected:
 
 	private:
 		/// <summary>
 		/// Variable del diseñador necesaria.
 		/// </summary>
-		System::ComponentModel::Container ^components;
+
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
@@ -48,12 +62,70 @@ namespace DiscRoomUPC {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"Formulario";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->components = (gcnew System::ComponentModel::Container());
+			this->timer1 = (gcnew System::Windows::Forms::Timer(this->components));
+			this->SuspendLayout();
+			// 
+			// timer1
+			// 
+			this->timer1->Enabled = true;
+			this->timer1->Tick += gcnew System::EventHandler(this, &Formulario::timer1_Tick);
+			// 
+			// Formulario
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(682, 653);
+			this->Name = L"Formulario";
+			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
+			this->Text = L"Formulario";
+			this->KeyDown += gcnew System::Windows::Forms::KeyEventHandler(this, &Formulario::TeclaPresionada);
+			this->KeyUp += gcnew System::Windows::Forms::KeyEventHandler(this, &Formulario::Formulario_KeyUp);
+			this->ResumeLayout(false);
+
 		}
 #pragma endregion
-	};
+	private: System::Void timer1_Tick(System::Object^  sender, System::EventArgs^  e) {
+		Graphics^g = this->CreateGraphics();
+		BufferedGraphicsContext^espacio = BufferedGraphicsManager::Current;
+		BufferedGraphics^buffer = espacio->Allocate(g, this->ClientRectangle);
+		//buffer->Graphics->Clear(Color::White);
+		objJuego->dibujarJuego(buffer->Graphics, bmpNivel1, bmpBatman);
+		buffer->Render(g);
+		delete buffer;
+		delete	espacio;
+		delete g;
+	
+	}
+	private: System::Void TeclaPresionada(System::Object^  sender, System::Windows::Forms::KeyEventArgs^  e) {
+		switch (e->KeyCode)
+		{
+		case Keys::Up:
+			objJuego->getObjJugador()->setDirecciones(Direcciones::Arriba);
+			break;
+		case Keys::Down:
+			objJuego->getObjJugador()->setDirecciones(Direcciones::Abajo);
+			break;
+		case Keys::Right:
+			objJuego->getObjJugador()->setDirecciones(Direcciones::Derecha);
+			break;
+		case Keys::Left:
+			objJuego->getObjJugador()->setDirecciones(Direcciones::Izquierda);
+			break;
+
+		default:
+			break;
+		}
+	}
+	private: System::Void Formulario_KeyUp(System::Object^  sender, System::Windows::Forms::KeyEventArgs^  e) {
+		switch (e->KeyCode)
+		{
+		default:
+			objJuego->getObjJugador()->setDirecciones(Direcciones::Ninguno);
+			break;
+		}
+	
+	
+	}
+};
 }
